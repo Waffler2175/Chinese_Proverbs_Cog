612cc017774787d44463fbe47d739811c042297f
Waffler2175 <jamesbryan9274@gmail.com>
2026-03-12 21:31:02 -0400

    "initial" commit

---
+++ mycog.py
@@ -0,0 +1,17 @@
+from redbot.core import commands, app_commands
+import discord
+from .proverbs import run 
+class MyCog(commands.Cog):
+    """Chinese-Proverbs"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @app_commands.command(name="proverb", description="Get a random Chinese proverb")
+    async def proverb(self, interaction: discord.Interaction):
+        count = 1
+        await run(interaction, count)
+        
+
+        
---
+++ proverbs.py
@@ -0,0 +1,17 @@
+import random
+import json
+
+async def run(interaction, count):
+    with open ('chengyu_data.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    entries = random.sample(data, count)
+    for entry in entries:
+        await interaction.response.send_message(
+            f"Chinese: {entry['Chinese']}\n"
+            f"Pinyin: {entry['Pinyin']}\n"
+            f"English Literal: {entry['EnglishLiteral']}\n"
+            f"English Figurative: {entry['EnglishFigurative']}"
+        )
+
+
